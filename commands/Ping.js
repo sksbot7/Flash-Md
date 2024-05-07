c0cb3c93b55224122978f9902bf9aa4a97ad6c81
sksbot7 <104474096+sksbot7@users.noreply.github.com>
2024-05-07 19:31:51 +0530

    Update Ping.js

--- commands/Ping.js
+++ commands/Ping.js
@@ -30,7 +30,7 @@ const { default: axios } = require('axios');
 france({ nomCom: 'ping',
     desc: 'To check ping',
     Categorie: 'General',
-    reaction: '🚨', 
+    reaction: '😌', 
     fromMe: 'true', 
 
        
@@ -38,7 +38,7 @@ france({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('*Pong!!*\n ```' + 2000 + '``` *ms*') 
+    return repondre('*oomb😹!!*\n ```' + 999 + '``` *ms*') 
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
